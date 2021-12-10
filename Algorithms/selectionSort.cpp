959f254e8c85c42973881c54c66a3ff869c3452a
Arpita Naval <navalarpita@gmail.com>
2021-12-10 22:57:59 +0530

    Add Selection Sort

---
+++ Algorithms/selectionSort.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+void selectionSort(int a[], int n){
+    for (int i = 0; i < n-1; i++)
+    {
+        int smallest = i;
+        for (int j = 0; j < n; j++)
+        {
+            if( a[j] < a[smallest]){
+                smallest = j;
+            }
+        }
+        swap(a[i], a[smallest]);
+        }
+        
+    }
+    
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+       cin>>a[i];
+    }
+    selectionSort(a,n);
+
+    for(int i = 0; i<n; i++){
+        cout<<a[i]<<" ";
+    }
+
+    return 0;
+}
