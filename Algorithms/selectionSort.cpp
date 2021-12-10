d87cc59e5e68343f4c12e1d40e9cb83e6085fa06
Arpita Naval <navalarpita@gmail.com>
2021-12-10 23:05:50 +0530

    Add Selection Sort

--- Algorithms/selectionSort.cpp
+++ Algorithms/selectionSort.cpp
@@ -5,7 +5,7 @@ void selectionSort(int a[], int n){
     for (int i = 0; i < n-1; i++)
     {
         int smallest = i;
-        for (int j = 0; j < n; j++)
+        for (int j = i+1; j < n; j++)
         {
             if( a[j] < a[smallest]){
                 smallest = j;
